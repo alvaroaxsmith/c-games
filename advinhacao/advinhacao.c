6a0b721545665ad49ecd433eb027b867edb1d348
alvaroun <alvaromachadoferreira@hotmail.com>
2022-01-02 18:51:49 -0300

    condicoes if & else

--- advinhacao/advinhacao.c
+++ advinhacao/advinhacao.c
@@ -6,8 +6,23 @@ int main() {
     printf("************************************\n");
 
     int chute;
+    int numerosecreto;
 
     printf("Qual e o seu chute? ");
     scanf("%d", &chute);
-    printf("Voce chutou o numero %d", chute);
+    printf("Seu chute foi: %d\n", chute);
+
+    
+    int acertou = chute == numerosecreto;
+
+    if (acertou) {
+        printf("Parabens! Voce acertou!\n");
+    } else {
+        int maior = chute > numerosecreto;
+        if (maior) {
+            printf("seu chute foi maior do que o numero secreto!\n");
+        } else {
+            printf("Seu chute foi menor do que o numero secreto!\n");
+        }
+    }
 }
