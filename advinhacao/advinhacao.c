cacd89821310f74174ca9145c01bd2efaf82959d
alvaroun <alvaromachadoferreira@hotmail.com>
2022-01-05 23:54:23 -0300

    adicionado tentativas ao jogo com while

--- advinhacao/advinhacao.c
+++ advinhacao/advinhacao.c
@@ -7,31 +7,29 @@ int main() {
     printf("************************************\n");
 
     int chute;
-    int numerosecreto;
-for(int i=1;i<=NUMERO_DE_TENTATIVAS; i++) {
-    printf("Qual e o seu %do. chute? ", i);
+    int numerosecreto = 42;
+    int ganhou = 0;
+    int tentativas = 1;
+while(!ganhou) {
+    printf("Qual e o seu %do. chute? ", tentativas);
     scanf("%d", &chute);
     if (chute < 0) {
         printf("Voce nao pode chutar numeros negativos\n");
-        i--;
-        continue
+        continue;
     }
-    printf("Seu %do. chute foi: %d\n", i, chute);
+    printf("Seu %do. chute foi: %d\n", tentativas, chute);
 
-    
     int acertou = chute == numerosecreto;
+    int maior = chute > numerosecreto;
 
     if (acertou) {
         printf("Parabens! Voce acertou!\n");
-        break;
-    } else {
-        int maior = chute > numerosecreto;
-        if (maior) {
-            printf("seu chute foi maior do que o numero secreto!\n");
+        ganhou = 1;
+    } else if(maior) {
+        printf("seu chute foi maior do que o numero secreto!\n");
         } else {
             printf("Seu chute foi menor do que o numero secreto!\n");
         }
     }
-}
-printf("Obrigado por jogar!\n");
+    tentativas++;
 }
