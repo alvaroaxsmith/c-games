6cb7799adf165f545cf3f0f8a7cfd1a709e0cb2a
alvaroun <alvaromachadoferreira@hotmail.com>
2022-01-18 23:17:41 -0300

    inciando jogo da forca

---
+++ forca/forca.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main() {
+ 
+    char palavrasecreta[20];
+    sprintf(palavrasecreta, "MELANCIA");
+ 
+    int acertou = 0;
+    int enforcou = 0;
+ 
+    do {
+        char chute;
+        
+        printf("Qual letra? ");
+        scanf(" %c", &chute);
+ 
+        for(int i = 0; i < strlen(palavrasecreta); i++) {
+            if(palavrasecreta[i] == chute) {
+                printf("A posição %d tem essa letra\n", i+1);
+            }
+        }
+    } while (!acertou && !enforcou);
+ 
+}
